3384087b179bfff1c444c608ca5aae3a5da2a8bb
Aiden Marques <aiden.marques94@gmail.com>
2020-10-04 19:24:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,21 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  require 'pry'
+  
+  def self.create
+    song = self.new
+    @@all << song
+    song 
+  end
 
+
+  def self.new_by_name
+    @@all.split
+    binding.pry
+  end  
+  
   def self.all
     @@all
   end
@@ -9,5 +23,20 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.new_by_name
+    
+  end 
+  
+  def self.create_from_file(file)
+    
+    format = file.split
+    format.collect do |files| files
+  end
+    
+  end 
 
+  def self.destroy_all
+    self.all.clear
+  end 
 end
