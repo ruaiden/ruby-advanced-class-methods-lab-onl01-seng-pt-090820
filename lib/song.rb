8de2b08e40c6647498b6515f9791b981e6df9ca5
Aiden Marques <aiden.marques94@gmail.com>
2020-10-04 19:25:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-  
-  require 'pry'
-  
-  def self.create
-    song = self.new
-    @@all << song
-    song 
-  end
 
-
-  def self.new_by_name
-    @@all.split
-    binding.pry
-  end  
-  
   def self.all
     @@all
   end
@@ -23,20 +9,60 @@ class Song
   def save
     self.class.all << self
   end
-  
-  def self.new_by_name
-    
-  end 
-  
-  def self.create_from_file(file)
-    
-    format = file.split
-    format.collect do |files| files
+
+  def self.create
+    song = Song.new
+    self.all<<song
+    song
   end
-    
-  end 
+
+  def self.new_by_name(song_name)
+    song = Song.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = Song.new
+    song.name = song_name
+    self.all<<song
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.find {|song| song.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    if self.find_by_name(song_name)
+      self.find_by_name(song_name)
+    else
+      self.create_by_name(song_name)
+    end
+  end
+
+  def self.alphabetical
+    alph_song_names = []
+    self.all.each {|song| alph_song_names << song.name}
+    alph_song_names.sort!
+    alph_song_names.map {|song_name| self.find_by_name(song_name)}
+  end
+
+  def self.new_from_filename(filename)
+    song_name = filename.split(" - ")[1][0..-5]
+    new_song = self.new_by_name(song_name)
+    new_song.artist_name = filename.split(" - ")[0]
+    new_song
+  end
+
+  def self.create_from_filename(filename)
+    song_name = filename.split(" - ")[1][0..-5]
+    new_song = self.create_by_name(song_name)
+    new_song.artist_name = filename.split(" - ")[0]
+    new_song
+  end
 
   def self.destroy_all
     self.all.clear
-  end 
+  end
 end
